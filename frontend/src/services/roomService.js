268ad39d48b0dcb544799113d6a8312d4330cc03
Peddireddy2005 <peddireddyeswarreddy9999@gmail.com>
2026-05-29 11:09:28 +0530

    room fix

--- frontend/src/services/roomService.js
+++ frontend/src/services/roomService.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+
 const API = import.meta.env.VITE_API_URL;
 
+console.log("ROOM SERVICE API:", API);
+
 export const getRoomsOfPG = async (pgId) => {
-    const response = await axios.get(`${API}/api/pgs/${pgId}/rooms`);
-    return response.data;
+  const response = await axios.get(`${API}/api/pgs/${pgId}/rooms`);
+  return response.data;
 };
