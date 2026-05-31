df7aad0931d28d735a0005b6b0b638e3763396ea
Peddireddy2005 <peddireddyeswarreddy9999@gmail.com>
2026-05-31 18:47:32 +0530

    Update allocate resident page

--- backend/models/Room.js
+++ backend/models/Room.js
@@ -1,40 +1,44 @@
 const mongoose = require("mongoose");
 
-const roomSchema = new mongoose.Schema({
-
+const roomSchema = new mongoose.Schema(
+  {
     roomNumber: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
 
     capacity: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
 
     occupancy: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
 
     rent: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
 
     pg: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "PG",
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "PG",
+      required: true,
     },
-    residents:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:"Resident"
-    }
 
-}, {
-    timestamps: true
-});
+    residents: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Room = mongoose.model("Room", roomSchema);
 
