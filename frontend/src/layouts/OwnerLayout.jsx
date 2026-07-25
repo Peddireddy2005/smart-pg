744069e78d14413da628fdc4efd96e648e8c4e49
Peddireddy2005 <peddireddyeswarreddy9999@gmail.com>
2026-07-25 22:53:25 +0530

    ui change

--- frontend/src/layouts/OwnerLayout.jsx
+++ frontend/src/layouts/OwnerLayout.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import toast from "react-hot-toast";
 import { Building2, DoorOpen, Users, LineChart, Wallet, Flag } from "lucide-react";
-import api from "../../services/api";
+import api from "../services/api";
 import { getSession } from "../services/authService";
 import ConfirmModal from "../components/ConfirmModal";
 
